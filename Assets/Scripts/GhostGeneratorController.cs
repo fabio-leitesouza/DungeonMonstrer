2f59e326ec870ebc3205a946de86dd612396a77b
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2024-09-25 15:07:30 -0300

    Update GhostGeneratorController.cs

--- Assets/Scripts/GhostGeneratorController.cs
+++ Assets/Scripts/GhostGeneratorController.cs
@@ -7,18 +7,18 @@ public class GhostGeneratorController : MonoBehaviour
     public GameObject Ghost;
     private float countTime = 0;
     public float TimeGeneratorGhost = 1;
+    private int countGhost = 0;
 
     void Update()
     {
         countTime += Time.deltaTime;
-        if (countTime >= TimeGeneratorGhost)
+        if (countTime >= TimeGeneratorGhost && countGhost < 10)
         {
             GeneratorNewGhost();
             countTime = 0;
-        }
-       
+            countGhost +=1 ;
+        }       
     }
-
     void GeneratorNewGhost()
     {
         Vector3 creationPoint = positionRandom();
@@ -26,8 +26,8 @@ public class GhostGeneratorController : MonoBehaviour
     }
     Vector3 positionRandom()
     {
-        Vector3 position = Random.insideUnitSphere * 5;
-        position += transform.position; 
+        Vector3 position = Random.insideUnitSphere * 5; //posicao aleatória de uma esfera de raio 5
+        position += transform.position; //adiciona a posição do gerador ao valor aleatório
         position.y = transform.position.y; //copia a posição do y de objeto gerador
         return position;
     }
